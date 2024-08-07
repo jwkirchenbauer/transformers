9b9948575704cfd2b2d219e158ef4fd57a104eb2
jwkirchenbauer <jkirchen@umd.edu>
2024-08-06 20:13:16 -0400

    oops lost some code cleaning the diff

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -312,6 +312,75 @@ class LlamaMLP(nn.Module):
         return down_proj
 
 
+class AsymmetricLlamaMLP(nn.Module):
+    def __init__(self, config, lit_mlp_config):
+        super().__init__()
+        self.config = config
+        self.hidden_size = config.hidden_size
+        self.intermediate_size = config.intermediate_size
+
+        self.lit_mlp_config = lit_mlp_config
+        # self.gate_proj = nn.Linear(
+        #     self.hidden_size, self.intermediate_size, bias=config.mlp_bias
+        # )
+        # self.up_proj = nn.Linear(
+        #     self.hidden_size, self.intermediate_size, bias=config.mlp_bias
+        # )
+        # self.down_proj = nn.Linear(
+        #     self.intermediate_size, self.hidden_size, bias=config.mlp_bias
+        # )
+        self.gate_proj = nn.Linear(
+            self.lit_mlp_config.n_embd_mlp_in,
+            self.lit_mlp_config.intermediate_size,
+            bias=config.mlp_bias,
+        )
+        self.up_proj = nn.Linear(
+            self.lit_mlp_config.n_embd_mlp_in,
+            self.lit_mlp_config.intermediate_size,
+            bias=config.mlp_bias,
+        )
+        self.down_proj = nn.Linear(
+            self.lit_mlp_config.intermediate_size,
+            self.lit_mlp_config.n_embd_mlp_out,
+            bias=config.mlp_bias,
+        )
+        self.act_fn = ACT2FN[config.hidden_act]
+
+    def forward(self, x):
+        if self.config.pretraining_tp > 1:
+            slice = self.intermediate_size // self.config.pretraining_tp
+            gate_proj_slices = self.gate_proj.weight.split(slice, dim=0)
+            up_proj_slices = self.up_proj.weight.split(slice, dim=0)
+            down_proj_slices = self.down_proj.weight.split(slice, dim=1)
+
+            gate_proj = torch.cat(
+                [
+                    F.linear(x, gate_proj_slices[i])
+                    for i in range(self.config.pretraining_tp)
+                ],
+                dim=-1,
+            )
+            up_proj = torch.cat(
+                [
+                    F.linear(x, up_proj_slices[i])
+                    for i in range(self.config.pretraining_tp)
+                ],
+                dim=-1,
+            )
+
+            intermediate_states = (self.act_fn(gate_proj) * up_proj).split(slice, dim=2)
+            down_proj = [
+                F.linear(intermediate_states[i], down_proj_slices[i])
+                for i in range(self.config.pretraining_tp)
+            ]
+            down_proj = sum(down_proj)
+        else:
+            down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+
+        return down_proj
+
+
+
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     """
     This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
